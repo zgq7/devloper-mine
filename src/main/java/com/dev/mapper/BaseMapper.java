f0674db560a7f62c3da375b5dc599a32abb28cbc
zgq7 <1140661106@qq.com>
2019-06-28 18:20:55 +0800

    office

--- src/main/java/com/dev/mapper/BaseMapper.java
+++ src/main/java/com/dev/mapper/BaseMapper.java
@@ -1,5 +1,6 @@
 package com.dev.mapper;
 
+import org.apache.ibatis.annotations.SelectProvider;
 import tk.mybatis.mapper.common.IdsMapper;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
@@ -10,4 +11,14 @@ import tk.mybatis.mapper.common.MySqlMapper;
  */
 public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T>, IdsMapper<T> {
 
+
+    @SelectProvider(type = BaseMapperTemplate.class, method = "selectMax")
+    public Integer selectMax();
+
+    class BaseMapperTemplate {
+        public String selectMax(String tableName, String pp) {
+            String sql = "select max(" + pp + ") from " + tableName + "";
+            return sql;
+        }
+    }
 }
